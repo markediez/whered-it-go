bb0aff118b270eaf4e8754597ee82a2eeab3a454
Mark Diez <markediez@gmail.com>
2017-09-04 12:27:27 -0400

    Add reminder to index page

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -28,7 +28,7 @@ class RemindersController < ApplicationController
 
     respond_to do |format|
       if @reminder.save
-        format.html { redirect_to @reminder, notice: 'Reminder was successfully created.' }
+        format.html { redirect_to root_url, notice: 'Reminder was successfully created.' }
         format.json { render :show, status: :created, location: @reminder }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class RemindersController < ApplicationController
   def update
     respond_to do |format|
       if @reminder.update(reminder_params)
-        format.html { redirect_to @reminder, notice: 'Reminder was successfully updated.' }
+        format.html { redirect_to root_url, notice: 'Reminder was successfully updated.' }
         format.json { render :show, status: :ok, location: @reminder }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class RemindersController < ApplicationController
   def destroy
     @reminder.destroy
     respond_to do |format|
-      format.html { redirect_to reminders_url, notice: 'Reminder was successfully destroyed.' }
+      format.html { redirect_to root_url, notice: 'Reminder was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,5 +2,6 @@ class SiteController < ApplicationController
   def index
     @categories = Category.all
     @transactions = Transaction.all
+    @reminders = Reminder.where archived: false
   end
 end
