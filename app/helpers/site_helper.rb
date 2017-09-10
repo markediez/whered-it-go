b82c31f89fed859f21e4e7c1ada003d6b8b1ba32
Mark Diez <markediez@gmail.com>
2017-09-10 15:04:19 -0400

    Add alpha version of reporting

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,2 +1,28 @@
 module SiteHelper
+  def get_total_budget
+    budget = 0
+    Category.all.map { |c| budget += c.budget unless c.budget.nil? }
+
+    return budget
+  end
+
+  def get_total_expense
+    expense = 0
+    Transaction.where(payment_type: :PAY).map { |t| expense += t.value }
+
+    return expense
+  end
+
+  def get_comment(budget, expense)
+    difference = budget - expense
+    if difference < 0
+      message = "#{number_to_currency(difference)} over budget"
+    elsif difference > 0
+      message = "#{number_to_currency(difference)} under budget"
+    else
+      message = "Just right"
+    end
+
+    return message
+  end
 end
