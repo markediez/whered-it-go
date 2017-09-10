1e01be3b9606abbc852425c4edf728f729e505e4
Mark Diez <markediez@gmail.com>
2017-09-10 17:53:45 -0400

    Add method to check if a transaction is a payment

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,4 +3,8 @@ class Transaction < ApplicationRecord
 	belongs_to :bill
 
 	validates :payment_type, inclusion: { in: %w(PAY EARN), message: "${value} is not a valid type" }
+
+	def payment?
+		return self.payment_type == "PAY"
+	end
 end
